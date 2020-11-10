93585623c30bfef52752c2d93d6d1b722c1f28e1
mgd-bilska <52111807+mgd-bilska@users.noreply.github.com>
2020-11-10 08:35:04 +0200

    updated answer retrieval

--- make-answer/make_answer_test.py
+++ make-answer/make_answer_test.py
@@ -7,21 +7,21 @@ sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 sparql.setReturnFormat(JSON)
 
 #read in the NN-translated queries
-queryFile = open('sparql-queries.txt', 'r') 
+queryFile = open('test-correct-sparql.txt', 'r', encoding = 'utf-8') 
 queries = queryFile.readlines() 
 queryFile.close()
 
-outputFile = open("answers.txt","w+")
+outputFile = open("answers-test.txt","w+")
 
 #take every query and find the answer
 for queryNo in range(len(queries)):
     sparqlQuery = queries[queryNo]
     sparqlQuery = re.sub('\\n$', '', sparqlQuery) #remove end of line character
-    # TODO: fix queries (e.g. get rid of spaces after underscores)
-    print(str(queryNo/len(queries)*100)+'%') # to see some progress
+    print(str(queryNo/len(queries)*100)+'%')
     sparql.setQuery(sparqlQuery)
-    results = sparql.query().convert() # find answer
-    outputFile.write(str(results)+'\n') # save answer
+    results = sparql.query().convert()
+    outputFile.write(str(results)+'\n').encode('utf8')
+    
 
     
 outputFile.close()
