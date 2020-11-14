433bebed0b2b35757940ed7dce383aa5c534c666
mgd-bilska <52111807+mgd-bilska@users.noreply.github.com>
2020-11-14 15:04:01 +0200

    updated the make_answer so that it works for the new brute-forced sparl-queries

--- make-answer/make_answer.py
+++ make-answer/make_answer.py
@@ -1,6 +1,10 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import re 
 import json
+import sys
+
+error_msg = 'ERROR: NOT ENOUGH ITEMS \n'
+hyphen = '-\n'
 
 #set the sparql wrapper for dbpedia
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
@@ -8,7 +12,7 @@ sparql.setReturnFormat(JSON)
 
 #read in the NN-translated queries
 queryFile = open('sparql-queries.txt', 'r') 
-queries = queryFile.readlines() 
+queries = queryFile.readlines()
 queryFile.close()
 
 outputFile = open("answers.txt","w+")
@@ -16,13 +20,18 @@ outputFile = open("answers.txt","w+")
 #take every query and find the answer
 for queryNo in range(len(queries)):
     sparqlQuery = queries[queryNo]
-    sparqlQuery = re.sub('\\n$', '', sparqlQuery) #remove end of line character
-    # TODO: fix queries (e.g. get rid of spaces after underscores)
-    print(str(queryNo/len(queries)*100)+'%') # to see some progress
-    sparql.setQuery(sparqlQuery)
-    results = sparql.query().convert() # find answer
-    outputFile.write(str(results)+'\n') # save answer
+    if sparqlQuery == hyphen:
+        outputFile.write('-\n')
+    elif sparqlQuery == error_msg:
+        outputFile.write('NO ANSWER DUE TO LACK OF QUERIES\n')
+    else:
+        sparqlQuery = re.sub(' _', '_', sparqlQuery)
+        sparqlQuery = re.sub(' >', '>', sparqlQuery)
+        sparqlQuery = re.sub('\\n$', '', sparqlQuery).encode('ascii', "ignore") #remove end of line character
+        sparql.setQuery(sparqlQuery)
+        answer = sparql.query().convert()
+        outputFile.write(str(answer)+'\n')
+    print(str(round(queryNo/len(queries)*100, 2))+'%')
 
     
 outputFile.close()
-
